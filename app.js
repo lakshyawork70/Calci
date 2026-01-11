2bdd5ffc17abe6578abbb8ada87964375644b195
lakshyawork70 <lakshyawork70@gmail.com>
2026-01-11 18:14:16 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const display = document.querySelector(".display");
+
+function addToDisplay(input){
+    display.value +=input;
+}
+
+function clearDisplay(){
+    display.value =""
+}
+
+function calculate(){
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "ERROR"
+    }
+}
